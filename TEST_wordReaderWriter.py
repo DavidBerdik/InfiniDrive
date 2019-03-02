2e6504bb852cc5584ce3c6b4db53ef12d4333670
David Berdik <berdikgit@theprogrammersworld.net>
2019-03-01 23:31:30 -0500

    Removed tests. They seemed like a good idea at the time but now I think they were not.

--- TEST_wordReaderWriter.py
+++
@@ -1,5 +0,0 @@
-from wordReaderWriter import writeWord
-from wordReaderWriter import readWord
-
-writeWord("./TEST/troll.png", "./TEST/test.docx")
-#readWord("./TEST/test.docx") # We don't really need this right now.
