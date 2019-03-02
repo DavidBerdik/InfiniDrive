8a4b93d9b19e6fc1b785910bc94204539df3246a
SMyrick98 <stevenmyrick98@gmail.com>
2019-03-02 02:36:27 -0500

    Added ability to upload Word Docs (.docx files) and included 3 test files

--- docs.py
+++ docs.py
@@ -2,6 +2,8 @@ from __future__ import print_function
 import pickle
 import os.path
 from googleapiclient.discovery import build
+from apiclient import errors
+from apiclient.http import MediaFileUpload
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 
@@ -12,17 +14,7 @@ SCOPES = ['https://www.googleapis.com/auth/drive']
 def del_folder(id):
     service.files().delete(fileId=id).execute()
 
-#CREATES FOLDER#
-def create_folder(service, file_path):
-    file_metadata = {
-    'name': file_path,
-    'mimeType': 'application/vnd.google-apps.folder'
-    }
-    file = service.files().create(body=file_metadata, fields= 'id').execute()
-    print('Folder created, ID: %s' % file.get('id'))
-    return file.get('id')
-
-#CREATES SERVICE OBJECT#
+#Creates Service object to allow interaction with Google Drive
 def get_service():
     """Shows basic usage of the Drive v3 API.
     Prints the names and ids of the first 10 files the user has access to.
@@ -66,10 +58,40 @@ def get_service():
 	    #Deletes things
             #service.files().delete(fileId=item['id']).execute()
 
+#Creates a folder and returns its ID
+def create_folder(service, file_path):
+    folder_metadata = {
+    'name': file_path,
+    'mimeType': 'application/vnd.google-apps.folder'
+    }
+    folder = service.files().create(body=folder_metadata, fields= 'id').execute()
+
+    print('Folder created, ID: %s' % folder.get('id'))
+    return folder.get('id')
+
+#Stores a file into a folder
+def store_doc(service, folderId, file_path):
+    file_metadata = {
+    'name': file_path,
+    'mimeType': 'application/vnd.google-apps.document',
+    'parents': [folderId]
+    }
+    media = MediaFileUpload(file_path, mimetype='application/vnd.openxmlformats-officedocument.wordprocessingml.document')
+    file = service.files().create(body=file_metadata,
+                                  media_body=media,
+                                  fields = 'id').execute()
+    
+    print('File created, ID: %s' % file.get('id'))
+
+#Returns folder id and service object for document insertion into the folder
 def begin_storage(file_path):
     service = get_service()
     folderId = create_folder(service, file_path)
     return service, folderId
+    
 
 if __name__ == '__main__':
     service, folderId = begin_storage('example/path')
+    store_doc(service, folderId, 'testdoc1.docx')
+    store_doc(service, folderId, 'testdoc2.docx')
+    store_doc(service, folderId, 'testdoc3.docx')
