64f5a866cebb20cf5c3cae63d8f6bdb9e8d9fba8
David Berdik <berdikgit@theprogrammersworld.net>
2019-03-01 19:57:33 -0500

    Added functions for writing Word documents with images and reading images from Word documents

---
+++ TEST_wordReaderWriter.py
@@ -0,0 +1,5 @@
+from wordReaderWriter import writeWord
+from wordReaderWriter import readWord
+
+writeWord("./TEST/troll.png", "./TEST/test.docx")
+#readWord("./TEST/test.docx") # We don't really need this right now.
