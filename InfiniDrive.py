4e8490ea1843cf83a5250deb058fa7ef686a5ec6
David Berdik <berdikgit@theprogrammersworld.net>
2020-04-19 12:57:17 -0400

    Changed version number from v1.0.16 to v1.0.17

--- InfiniDrive.py
+++ InfiniDrive.py
@@ -13,7 +13,7 @@ from libs.uploadHandler import handle_upload_fragment
 
 progress = getpatchedprogress()
 
-version = "1.0.16"
+version = "1.0.17"
 
 # Open debugging log for writing
 debug_log = open("log.txt", "w")
