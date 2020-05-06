83e0bd10945050312da37b7d59e483082d2e3e1c
David Berdik <berdikgit@theprogrammersworld.net>
2020-05-05 21:55:23 -0400

    Modified "missing modules" error message

--- libs/requirements.py
+++ libs/requirements.py
@@ -16,24 +16,25 @@ class requirements:
 			from tabulate import tabulate
 			from libs.uploadHandler import handle_upload_fragment
 		except (ModuleNotFoundError, ImportError) as error:
-			print("Oops! ", end = " ")
+			print('\nOops! ', end = ' ')
 			print(error)
-			print("To install all the libraries required by Infinidrive. If you use pip, execute one of the following commands from the root InfiniDrive directory:")
-			print("1. pip install -r requirements.txt\n2. python -m pip install -r -requirements.txt")
-			print("For more Information refer to Readme")
+			print('\nOne or more InfiniDrive dependencies are not installed on your system.')
+			print('Using pip, you can install these dependencies using one of the following commands from the root InfiniDrive directory:')
+			print('\t1. pip install -r requirements.txt\n\t2. python -m pip install -r requirements.txt')
+			print('\nMore information is available in \'README.md\' as well as online at https://github.com/DavidBerdik/InfiniDrive')
 			return False
 		return True
 
 	def check_credentials(self):
 		import os
 		if not os.path.exists('credentials.json'):
-			print('InfiniDrive could not start because you have not provided a "credentials.json" file.')
-			print('Please do so and try again. Instructions for doing this are available in "README.md"')
+			print('InfiniDrive could not start because you have not provided a \'credentials.json\' file.')
+			print('Please do so and try again. Instructions for doing this are available in \'README.md\'')
 			print('as well as online at https://github.com/DavidBerdik/InfiniDrive')
 			return False
 		elif not os.path.exists('token.pickle'):
-			print("Please complete account authentication using the following URL.")
-			print("You can then run your previous command again.\n")
+			print('Please complete account authentication using the following URL.')
+			print('You can then run your previous command again.\n')
 			import libs.driveAPI as driveAPI
 			driveAPI.get_service()
 			return False
