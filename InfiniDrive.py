c9ad762dfb0fde0027ea9efdb0edecc363738113
David Berdik <berdikgit@theprogrammersworld.net>
2020-05-15 19:09:15 -0400

    Changed version number from v1.0.18 to v1.0.19

--- InfiniDrive.py
+++ InfiniDrive.py
@@ -14,7 +14,7 @@ from tabulate import tabulate
 
 class InfiniDrive:
 	def __init__(self):
-		self.version = "1.0.18"
+		self.version = "1.0.19"
 		self.debug_log = open("log.txt", "w")
 		self.debug_log.write("Version: " + self.version + "\n\n")
 		self.progress = getpatchedprogress()
