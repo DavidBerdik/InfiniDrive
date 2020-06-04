91d78728ec55e02eb398664d343ee07725428610
David Berdik <berdikgit@theprogrammersworld.net>
2020-06-04 18:14:45 -0400

    Added support for listing InfiniDrive files via FTP server

--- libs/ftp_server.py
+++ libs/ftp_server.py
@@ -1,9 +1,42 @@
+import libs.driveAPI as driveAPI
+
 from pyftpdlib.authorizers import DummyAuthorizer
+from pyftpdlib.handlers import BufferedIteratorProducer
 from pyftpdlib.handlers import FTPHandler
 from pyftpdlib.servers import FTPServer
 
+drive_service = driveAPI.get_service()
+
 class InfiniDriveFTPHandler(FTPHandler):
-	pass
+	def ftp_LIST(self, path):
+		# Get list of files
+		remote_files = [item for sublist in driveAPI.list_files(drive_service) for item in sublist]
+		iterator = self.format_file_list(remote_files)
+		producer = BufferedIteratorProducer(iterator)
+		self.push_dtp_data(producer, isproducer=True, cmd="LIST")
+		return path
+
+	def ftp_NLST(self, path):
+		# Get list of files
+		return self.ftp_LIST(path)
+	
+	def ftp_STAT(self, path):
+		# Get list of files
+		return self.ftp_LIST(path)
+
+	def ftp_MLSD(self, path):
+		# Get list of files
+		return self.ftp_LIST(path)
+
+	def ftp_MLST(self, path):
+		# Get list of files
+		return self.ftp_LIST(path)
+
+	def format_file_list(self, files):
+		# Emulates the output of "/bin/ls -lA" but fakes all other data for compatibility.
+		for file in files:
+			line = "-rwxrwxrwx   1 owner   group          0 Jan 01  0:00 " + file + "\r\n"
+			yield line.encode('utf8')
 
 def init_ftp_server(user='user', password='password', port=21):
 	# Initializes the FTP server that interfaces with InfiniDrive
